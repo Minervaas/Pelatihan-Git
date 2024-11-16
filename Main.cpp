dbeb9d20a2065ea8f73ae05cf591b87332d53b12
Ikhsan Firdaus <ikhsanf1555@gmail.com>
2024-11-16 16:46:07 +0700

    initial commit

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int man()
+{
+    system ("cls");
+
+    cout << "Hello World" << endl;
+    cout << "this is only for dev branch" << endl;
+
+    return 0;
+}
